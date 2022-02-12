acf2bb6fbf88a0e5364751e889735f8992f31b95
Bartlomiej <bartlomiej.bujak95@gmail.com>
2022-02-12 15:16:52 +0100

    added footer and fixed display of burger button on white background

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const nav = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	if (!nav.classList.contains('nav--active')) {
@@ -10,6 +13,7 @@ const handleNav = () => {
 }
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
+	navBtnBars.classList.remove('black-bars-color')
 	navItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
 		item.style.animationDelay = '.' + delayTime + 's'
@@ -22,4 +26,21 @@ navItems.forEach(item => {
 		handleNavItemsAnimation()
 	})
 })
+const handleCurrentYear = () => {
+	let year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+const handleObserver = () => {
+	const currentSection = window.scrollY
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+handleCurrentYear()
 burgerBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
